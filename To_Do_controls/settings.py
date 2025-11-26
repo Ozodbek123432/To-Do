d509176ae4964ee37d7b13c08b476fa04c32a5c5
cyuber.devoloper <sabdumajid91@gmail.com>
2025-11-26 20:55:40 +0500

    Add STATIC_ROOT to settings.py

--- To_Do_controls/settings.py
+++ To_Do_controls/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'To_Do_controls.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],           # yoki os.path.join(BASE_DIR, 'templates')
+            'DIRS': [BASE_DIR / 'templates'],           # yoki os.path.join(BASE_DIR, 'templates')
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,7 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
