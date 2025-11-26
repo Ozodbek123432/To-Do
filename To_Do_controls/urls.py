cb3f86d1d7ac73100dcab308387f42f95f08cc0a
cyuber.devoloper <sabdumajid91@gmail.com>
2025-11-26 21:50:59 +0500

    Add gunicorn to requirement

--- To_Do_controls/urls.py
+++ To_Do_controls/urls.py
@@ -9,24 +9,25 @@ from django.urls import path, include
 #     path('', home),
 #
 # ]
-# from django.contrib import admin
-# from django.urls import path
-# from django.http import HttpResponse
-#
-# def home_view(request):
-#     return HttpResponse("""
-#     <h1 style="text-align:center; margin-top:100px; color:green;">
-#         SAYT JONLI ISHLAYAPTI!
-#     </h1>
-#     <p style="text-align:center; font-size:20px;">
-#         Railway’da muvaffaqiyatli deploy qilindi!<br><br>
-#         Admin panel: <a href="/admin">/admin</a><br><br>
-#         Keyingi qadamlar uchun tayyor!
-#     </p>
-#     """)
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("""
+    <h1 style="text-align:center; margin-top:100px; color:green;">
+        SAYT JONLI ISHLAYAPTI! 
+    </h1>
+    <p style="text-align:center; font-size:20px;">
+        Railway’da muvaffaqiyatli deploy qilindi!<br><br>
+        Admin panel: <a href="/admin">/admin</a><br><br>
+        <a href="/todo">/</a><br><br>
+        Keyingi qadamlar uchun tayyor!
+    </p>
+    """)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("todo/", include("ToDo_beckend.urls")),   # asosiy sahifa – hech qanday app kerak emas
-    path('', include("accaounts.urls")),
+    path('', home_view),   # asosiy sahifa – hech qanday app kerak emas
+    path("todo/", include("ToDo_beckend.urls")),
 ]
